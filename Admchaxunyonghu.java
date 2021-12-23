0c455b3c9e65983083b926f2dce5f638e2b9d01b
shuangmuchenglin <1476154032@qq.com>
2021-12-23 13:04:49 +0800

    the second commit

--- Admchaxunyonghu.java
+++ Admchaxunyonghu.java
@@ -9,7 +9,7 @@ import javax.swing.table.*;
 
 public class Admchaxunyonghu implements ActionListener
 {
-	Font font= new Font("宋体",Font.BOLD,12);//添加字体font
+    Font font= new Font("宋体",Font.BOLD,12);//添加字体font
     JFrame f=new JFrame("用户查询");
     String[] names;
     int i,j,RowNum,ColNum;
@@ -18,42 +18,42 @@ public class Admchaxunyonghu implements ActionListener
     TextField edit=new TextField(10);
     JLabel label=new JLabel("输入用户姓名：");
     JButton b=new JButton("查询");
-    
-	public static void main(String args[]) 
-	{
-		new Admchaxunyonghu();		
-	}
-	
-//响应按钮的事件，完成查询功能
-	public void actionPerformed(ActionEvent e)
-	{
-          try{
-		
-			
-        	//装载驱动程序
-        	  Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-       
+
+    public static void main(String args[])
+    {
+        new Admchaxunyonghu();
+    }
+
+    //响应按钮的事件，完成查询功能
+    public void actionPerformed(ActionEvent e)
+    {
+        try{
+
+
+            //装载驱动程序
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
             //创建连接
-            Connection con=DriverManager.getConnection 
-            		("jdbc:sqlserver://localhost:1433;DatabaseName=nnn","sa","123456");      
-		
+            Connection con=DriverManager.getConnection
+                    ("jdbc:sqlserver://localhost:1433;DatabaseName=nnn","sa","123456");
+
             Statement stmt=con.createStatement();
             String name=edit.getText();
             ResultSet rs=stmt.executeQuery("select * from readerinformation where Readername="+"'"+name+"'");
             // 清除原有行
             DefaultTableModel tableModel= (DefaultTableModel)table.getModel();
-            tableModel.setRowCount(0); 
+            tableModel.setRowCount(0);
             //添加查询到的记录集
             while (rs.next())
             {
-                 String[] arr=new String[6];
-                 arr[0]=rs.getString("Readerid");
-                 arr[1]=rs.getString("Readername");
-                 arr[2]=rs.getString("Readerage");
-                 arr[3]=rs.getString("Readertel");
-                 arr[4]=rs.getString("Password");
-                 arr[5]=rs.getString("ReaderTpye");
-                 tableModel.addRow(arr);
+                String[] arr=new String[6];
+                arr[0]=rs.getString("Readerid");
+                arr[1]=rs.getString("Readername");
+                arr[2]=rs.getString("Readerage");
+                arr[3]=rs.getString("Readertel");
+                arr[4]=rs.getString("Password");
+                arr[5]=rs.getString("ReaderTpye");
+                tableModel.addRow(arr);
             }
 
             //刷新表格，即重新绘制
@@ -62,36 +62,36 @@ public class Admchaxunyonghu implements ActionListener
             rs.close();
             stmt.close();
             con.close();
-            
-		}
-		catch(Exception ex)
-	    {
-	    	System.out.println(ex.getMessage());
-	    }
-	}
 
-	public Admchaxunyonghu()
-	{
-         try{
-					
-        	 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Connection con=DriverManager.getConnection             
-            		("jdbc:sqlserver://localhost:1433;DatabaseName=nnn","sa","123456");      
-		
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Admchaxunyonghu()
+    {
+        try{
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection con=DriverManager.getConnection
+                    ("jdbc:sqlserver://localhost:1433;DatabaseName=nnn","sa","123456");
+
             Statement stmt=con.createStatement();
-		    //构造JTable
+            //构造JTable
             ResultSet rs=stmt.executeQuery("select count(*) from readerinformation");
             rs.next();
             RowNum=rs.getInt(1);
-                         
-            rs=stmt.executeQuery("select * from readerinformation");
-            ResultSetMetaData rsmd=rs.getMetaData();              
 
-            
+            rs=stmt.executeQuery("select * from readerinformation");
+            ResultSetMetaData rsmd=rs.getMetaData();
+
+
             ColNum=rsmd.getColumnCount();
-  
-             names=new String[ColNum];     
-             for (i=1;i<=ColNum;i++) names[i-1]=rsmd.getColumnName(i);
+
+            names=new String[ColNum];
+            for (i=1;i<=ColNum;i++) names[i-1]=rsmd.getColumnName(i);
 
             info=new Object[RowNum][];
             i=0;
@@ -100,18 +100,18 @@ public class Admchaxunyonghu implements ActionListener
                 info[i]=new Object[ColNum];
                 for (j=1;j<=ColNum;j++)
                 {
-                   info[i][j-1]=rs.getObject(j);
+                    info[i][j-1]=rs.getObject(j);
                 }
                 i++;
             }
-            
+
             DefaultTableModel model = new DefaultTableModel(info,names);
-           
-            table=new JTable(model); 
+
+            table=new JTable(model);
             table.setBackground(new Color(255,250,205));
             table.setFont(font);
             table.setPreferredScrollableViewportSize(new Dimension(400,50));
- 
+
             b.addActionListener(this);
             Panel panel=new Panel();
             panel.add(label);
@@ -122,21 +122,21 @@ public class Admchaxunyonghu implements ActionListener
             //响应窗口的关闭事件
             f.addWindowListener(new WindowAdapter()
             {
-            		public void windowClosiing(WindowEvent ew)
-            	    {System.exit(0);}
-            		});
+                public void windowClosiing(WindowEvent ew)
+                {System.exit(0);}
+            });
             f.setVisible(true);
             f.setSize(700,400);
             f.setLocation(500,300);
             rs.close();
             stmt.close();
             con.close();
-            
-		}
-		catch(Exception e)
-	    {
-	    	System.out.println(e.getMessage());
-	    }
-        
-	}
+
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
